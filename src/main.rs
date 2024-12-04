6e7bcd797aa08f0ebd7fe6defd0cf978191b72bb
Petter Nilsson <pettni@umich.edu>
2024-12-04 12:38:11 +0300

    Optimize day 4.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use clap::{Parser, Subcommand};
+use aoc2024::Answer;
+use clap::{ArgAction, Parser, Subcommand};
 use std::fs;
+use std::hint::black_box;
 use std::path::PathBuf;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 #[cfg(feature = "dhat-heap")]
 #[global_allocator]
@@ -17,7 +19,7 @@ struct Args {
 #[derive(Subcommand)]
 enum Commands {
     Run(RunArgs),
-    RunAll,
+    RunAll(RunAllArgs),
 }
 
 #[derive(Parser)]
@@ -25,13 +27,46 @@ struct RunArgs {
     pub day: u32,
     #[arg(short, long, default_value=None)]
     pub input: Option<PathBuf>,
+    #[arg(long, short, action=ArgAction::SetTrue)]
+    pub benchmark: bool,
+}
+
+#[derive(Parser)]
+struct RunAllArgs {
+    #[arg(long, short, action=ArgAction::SetTrue)]
+    pub benchmark: bool,
 }
 
 fn get_default_data_path(day: u32) -> PathBuf {
     PathBuf::from(format!("data/{:02}.txt", day))
 }
 
-fn main_run(args: &RunArgs) -> Result<(), Box<dyn std::error::Error>> {
+fn part_run(f: impl Fn(&str) -> Answer, input: &str, benchmark: bool) -> (Answer, Duration, u128) {
+    let t0 = Instant::now();
+    let answer = {
+        #[cfg(feature = "dhat-heap")]
+        let _profiler = dhat::Profiler::new_heap();
+        f(input)
+    };
+    let dt0 = t0.elapsed();
+
+    if !benchmark {
+        (answer, dt0, 1)
+    } else {
+        let n = (Duration::from_secs(1).as_nanos() / dt0.as_nanos().max(10)).clamp(10, 10000);
+        let mut timers: Vec<Duration> = vec![];
+        for _ in 0..n {
+            let t0 = Instant::now();
+            black_box(f(black_box(input)));
+            timers.push(t0.elapsed());
+        }
+        let dt_nanos = timers.iter().map(|d| d.as_nanos()).sum::<u128>() / timers.len() as u128;
+        let dt = Duration::from_nanos(dt_nanos as u64);
+        (answer, dt, n)
+    }
+}
+
+fn main_run(args: &RunArgs) -> Result<Duration, Box<dyn std::error::Error>> {
     let (part_a, part_b) = aoc2024::solutions::ALL
         .get(args.day.saturating_sub(1) as usize)
         .unwrap_or_else(|| panic!("Invalid day {}", args.day));
@@ -40,39 +75,33 @@ fn main_run(args: &RunArgs) -> Result<(), Box<dyn std::error::Error>> {
         .input
         .clone()
         .unwrap_or_else(|| get_default_data_path(args.day));
-    let file_content = fs::read_to_string(&path);
-    if file_content.is_err() {
-        println!("Could not read data at {:?}", &path);
-        return Ok(());
-    }
+    let data = fs::read_to_string(&path).unwrap_or_else(|_| panic!("Couldn't open file"));
 
-    let data = file_content.unwrap();
+    let (out_a, dt_a, n_a) = part_run(part_a, data.as_str(), args.benchmark);
+    let dt_a_ms = dt_a.as_secs_f64() * 1e3;
+    println!("Part a: {out_a:<16} {dt_a_ms:>10.3}ms [N={n_a}]");
 
-    #[cfg(feature = "dhat-heap")]
-    let _profiler = dhat::Profiler::new_heap();
+    let (out_b, dt_b, n_b) = part_run(part_b, data.as_str(), args.benchmark);
+    let dt_b_ms = dt_b.as_secs_f64() * 1e3;
+    println!("Part b: {out_b:<16} {dt_b_ms:>10.3}ms [N={n_b}]");
 
-    let t0 = Instant::now();
-    let out = part_a(data.as_str());
-    let t1 = t0.elapsed();
-    println!("Part a: {out:<20} [{t1:.0?}]");
-
-    let t0 = Instant::now();
-    let out = part_b(data.as_str());
-    let t1 = t0.elapsed();
-    println!("Part b: {out:<20} [{t1:.0?}]");
-
-    Ok(())
+    Ok(dt_a + dt_b)
 }
 
-fn main_run_all() -> Result<(), Box<dyn std::error::Error>> {
+fn main_run_all(args: &RunAllArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let mut total_duration: Duration = Duration::default();
     for day in 1..aoc2024::solutions::ALL.len() + 1 {
         let args = RunArgs {
             day: day as u32,
             input: None,
+            benchmark: args.benchmark,
         };
         println!("Running day {day:02}");
-        main_run(&args)?;
+        let day_duration = main_run(&args)?;
+        total_duration += day_duration;
     }
+    println!("{:=>40}", "");
+    println!("Total duration: {total_duration:.3?}");
 
     Ok(())
 }
@@ -80,8 +109,8 @@ fn main_run_all() -> Result<(), Box<dyn std::error::Error>> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     match &args.command {
-        Commands::Run(cmd_args) => main_run(cmd_args)?,
-        Commands::RunAll => main_run_all()?,
+        Commands::Run(cmd_args) => main_run(cmd_args).map(|_| ())?,
+        Commands::RunAll(cmd_args) => main_run_all(cmd_args)?,
     }
 
     Ok(())
--- src/solutions/day_04.rs
+++ src/solutions/day_04.rs
@@ -1,80 +1,62 @@
-use crate::parsing::{identity, parse_matrix};
 use crate::Answer;
+use rayon::prelude::*;
 
 pub fn part_a(input: &str) -> Answer {
-    let (_, m) = parse_matrix(input, identity).unwrap();
-
-    let check_idx =
-        |i0: (usize, usize), i1: (usize, usize), i2: (usize, usize), i3: (usize, usize)| -> bool {
-            let a = m[i0.0][i0.1];
-            let b = m[i1.0][i1.1];
-            let c = m[i2.0][i2.1];
-            let d = m[i3.0][i3.1];
-            if a == 'X' && b == 'M' && c == 'A' && d == 'S' {
-                return true;
-            }
-            if a == 'S' && b == 'A' && c == 'M' && d == 'X' {
-                return true;
-            }
-            false
-        };
+    let m: Vec<_> = input.lines().collect();
 
     // iterate over starting index
     let nrows = m.len();
     let ncols = m[0].len();
-    let mut count = 0;
-    for i in 0..nrows {
+
+    // seems fastest to parallelize over rows
+    let row_counter = |i: usize| {
+        let mut count = 0;
         for j in 0..ncols {
-            // vertical
-            if i + 3 < nrows && check_idx((i, j), (i + 1, j), (i + 2, j), (i + 3, j)) {
-                count += 1;
-            }
-            // horizontal
-            if j + 3 < ncols && check_idx((i, j), (i, j + 1), (i, j + 2), (i, j + 3)) {
-                count += 1;
-            }
-            // diagonal tl-br
-            if i + 3 < nrows
-                && j + 3 < ncols
-                && check_idx((i, j), (i + 1, j + 1), (i + 2, j + 2), (i + 3, j + 3))
-            {
-                count += 1;
-            }
-            // diagonal tr-bl
-            if i + 3 < nrows
-                && j >= 3
-                && check_idx((i, j), (i + 1, j - 1), (i + 2, j - 2), (i + 3, j - 3))
-            {
-                count += 1;
+            for dx in -1..=1 {
+                for dy in -1..=1 {
+                    let is_xmas = "XMAS".bytes().zip(0isize..).all(|(b, k)| -> bool {
+                        let el_at_ii_jj = i
+                            .checked_add_signed(dx * k)
+                            .and_then(|ii| m.get(ii))
+                            .and_then(|row| {
+                                j.checked_add_signed(dy * k)
+                                    .and_then(|jj| row.as_bytes().get(jj))
+                            });
+                        el_at_ii_jj == Some(&b)
+                    });
+                    if is_xmas {
+                        count += 1;
+                    }
+                }
             }
         }
-    }
+        count
+    };
 
+    let count = (0..nrows).into_par_iter().map(row_counter).sum();
     Answer::Number(count)
 }
 
 pub fn part_b(input: &str) -> Answer {
-    let (_, m) = parse_matrix(input, identity).unwrap();
+    let m: Vec<_> = input.lines().collect();
 
-    let nrows = m.len();
-    let ncols = m[0].len();
-
-    let mut count = 0;
-    for i in 0..nrows - 2 {
-        for j in 0..ncols - 2 {
-            if m[i + 1][j + 1] != 'A' {
-                continue;
-            }
-            let diag1 = (m[i][j] == 'M' && m[i + 2][j + 2] == 'S')
-                || (m[i][j] == 'S' && m[i + 2][j + 2] == 'M');
-            let diag2 = (m[i + 2][j] == 'M' && m[i][j + 2] == 'S')
-                || (m[i + 2][j] == 'S' && m[i][j + 2] == 'M');
-            if diag1 && diag2 {
-                count += 1;
+    let row_counter = |i: usize| {
+        let mut row_count = 0;
+        for j in 0..m[i].len() - 2 {
+            let r0 = m[i].as_bytes();
+            let r1 = m[i + 1].as_bytes();
+            let r2 = m[i + 2].as_bytes();
+            if (r1[j + 1] == b'A')
+                && ((r0[j] == b'M' && r2[j + 2] == b'S') || (r0[j] == b'S' && r2[j + 2] == b'M'))
+                && ((r2[j] == b'M' && r0[j + 2] == b'S') || (r2[j] == b'S' && r0[j + 2] == b'M'))
+            {
+                row_count += 1;
             }
         }
-    }
+        row_count
+    };
 
+    let count = (0..m.len() - 2).into_par_iter().map(row_counter).sum();
     Answer::Number(count)
 }
 
@@ -82,18 +64,16 @@ pub fn part_b(input: &str) -> Answer {
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = r#"
-    MMMSXXMASM
-    MSAMXMSMSA
-    AMXSXMAAMM
-    MSAMASMSMX
-    XMASAMXAMM
-    XXAMMXXAMA
-    SMSMSASXSS
-    SAXAMASAAA
-    MAMMMXMMMM
-    MXMXAXMASX
-    "#;
+    const TEST_INPUT: &str = r#"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"#;
 
     #[test]
     fn test_part_a() {
